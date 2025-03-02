269f31055420f203024d177b780948440d649289
TAN PO HUI <phxx04@gmail.com>
2025-03-02 21:55:26 +0800

    / expo route / href fix

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,9 +11,9 @@ export default function Index() {
       }}
     >
       <Link href="/sign-in">Sign In</Link>
-      <Link href="/root/(tabs)/explore">Explore</Link>
-      <Link href="/root/(tabs)/profile">Profile</Link>
-      <Link href="/root/properties/1">Property</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
     </View>
   );
 }
